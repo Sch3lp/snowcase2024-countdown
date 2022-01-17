41f5a5dd065ebd94e8acd35b13ac3668e64de704
sch3lp <sch3lp@gmail.com>
2022-01-17 08:45:23 +0100

    trying _includes

--- js/bundle.js
+++ js/bundle.js
@@ -1,9 +1,3 @@
 ---
-dir_path: scripts/
-scripts:
-  - countdown.min.js
 ---
-{% for script in scripts %}
-    {% assign script_path = dir_path | append: script %}
-    {% include_relative script_path %}
-{% endfor %}
+{% include /countdown.min.js %}
